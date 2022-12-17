c90479c2ac1b33ebf4eed4fc3b9e08e6fe7b6556
Ole Holgernes <ole.holgernes@gmail.com>
2022-12-17 09:25:50 +0100

    add day13 part2 solution

--- day13/day_thirteen.py
+++ day13/day_thirteen.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 dataset = []
 s = 0
 
@@ -12,27 +14,27 @@ with open("inp.txt", "r") as inp:
 def check_equality(term1, term2):
     if isinstance(term1, int) and isinstance(term2, int):
         if term1 < term2:
-            return 1
+            return -1
         elif term1 == term2:
             return 2
         else:
-            return 3
+            return 1
     if isinstance(term1, list) and isinstance(term2, list):
         i = 0
         while i < len(term1) and i < len(term2):
             r = check_equality(term1[i], term2[i])
-            if r == 1:
-                return 1
+            if r == -1:
+                return -1
             elif r == 2:
                 i += 1
                 continue
-            elif r == 3:
-                return 3
+            elif r == 1:
+                return 1
             i += 1
         if i == len(term1) and i < len(term2):
-                return 1
+                return -1
         elif i == len(term2) and i < len(term1):
-                return 3
+                return 1
         else:
             return 2
     elif isinstance(term1, list):
@@ -42,7 +44,28 @@ def check_equality(term1, term2):
 
 for idx, pair in enumerate(dataset):
 
-    if check_equality(*pair) == 1:
+    if check_equality(*pair) == -1:
         s += (idx + 1)
 
 print(s)
+
+# part 2
+
+arr = []
+
+for pair in dataset:
+    arr.append(pair[0])
+    arr.append(pair[1])
+arr.append([[2]])
+arr.append([[6]])
+
+# comparator should return -1 or 1
+sorted_arr = sorted(arr, key=cmp_to_key(check_equality))
+
+#print(*sorted_arr, sep="\n")
+res = 1
+for idx, line in enumerate(sorted_arr):
+    if line == [[2]] or line == [[6]]:
+        res *= (idx + 1)
+
+print(res)
